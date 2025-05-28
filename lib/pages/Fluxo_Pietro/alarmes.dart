e9fd4f87a6a381445f7ec9d3d8fd1f5875ca736c
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2025-05-28 16:09:27 -0300

    Atualização
    Pagina Alarmes criada

---
+++ lib/pages/Fluxo_Pietro/alarmes.dart
@@ -0,0 +1,203 @@
+import 'package:estaciona_facil/components/modal_confirmacao.dart';
+import 'package:estaciona_facil/components/widget_label.dart';
+import 'package:estaciona_facil/pages/Fluxo_Pietro/minha_conta_dois.dart';
+import 'package:flutter/material.dart';
+
+class Alarmes extends StatefulWidget {
+  const Alarmes({super.key});
+
+  @override
+  State<Alarmes> createState() => _AlarmesState();
+}
+
+class _AlarmesState extends State<Alarmes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              WidgetLabel(texto: 'Minha Conta'),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      spacing: 4,
+                      children: [
+                        Icon(
+                          Icons.person,
+                          color: Color.fromARGB(255, 42, 42, 42),
+                          size: 30,
+                        ),
+                        Text(
+                          'Dados Gerais',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 42, 42, 42),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MinhaContaDois(),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          Icons.edit_square,
+                          color: Color.fromARGB(255, 42, 42, 42),
+                          size: 25,
+                        ),
+                        Text(
+                          'Alterar Senha',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 42, 42, 42),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                height: MediaQuery.of(context).size.height * 0.005,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text('Meus Dados', style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 30),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Column(
+                        spacing: 15,
+                        children: [
+                          Icon(
+                            Icons.person,
+                            color: Color.fromARGB(255, 42, 42, 42),
+                            size: 30,
+                          ),
+
+                          Icon(
+                            Icons.email,
+                            color: Color.fromARGB(255, 42, 42, 42),
+                            size: 30,
+                          ),
+
+                          Icon(
+                            Icons.call,
+                            color: Color.fromARGB(255, 42, 42, 42),
+                            size: 30,
+                          ),
+
+                          Icon(
+                            Icons.contact_emergency,
+                            color: Color.fromARGB(255, 42, 42, 42),
+                            size: 28,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 20,
+                        children: [
+                          Text(
+                            'Nome do usuário',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 42, 42, 42),
+                              fontSize: 17,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Arsenal',
+                            ),
+                          ),
+
+                          Text(
+                            'Email do usuário',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 42, 42, 42),
+                              fontSize: 17,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Arsenal',
+                            ),
+                          ),
+
+                          Text(
+                            'Numero de telefone',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 42, 42, 42),
+                              fontSize: 17,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Arsenal',
+                            ),
+                          ),
+
+                          Text(
+                            'CPF ou CNPJ',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 42, 42, 42),
+                              fontSize: 17,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 90),
+              ModalConfirmacao(texto: 'Salvar'),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.222,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/Logo.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -7,6 +7,7 @@ import 'package:estaciona_facil/pages/Fluxo_Manu/fluxo_login/inicio.dart';
 import 'package:estaciona_facil/pages/Fluxo_Manu/fluxo_recuperar.dart/recuperar_senha.dart';
 import 'package:estaciona_facil/pages/Fluxo_Joao/cadastro_um.dart';
 import 'package:estaciona_facil/pages/Fluxo_Pietro/adicionar_saldo_um.dart';
+import 'package:estaciona_facil/pages/Fluxo_Pietro/alarmes.dart';
 import 'package:estaciona_facil/pages/Fluxo_Pietro/historico.dart';
 import 'package:estaciona_facil/pages/Fluxo_Pietro/minha_conta_um.dart';
 import 'package:flutter/material.dart';
@@ -70,6 +71,8 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                           SizedBox(height: 10),
                           BotaoBasico(texto: 'Historico', pagina: Historico()),
+                          SizedBox(height: 10),
+                          BotaoBasico(texto: 'Alarmes', pagina: Alarmes()),
                           Text(
                             'João',
                             style: Theme.of(context).textTheme.titleLarge,
