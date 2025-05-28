17b5f3db514ceba53cdf8cc4f7146048b04019c7
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2025-05-28 16:53:16 -0300

    Atualização
    Pagina alarmes concluida, Radio funcionando.

--- lib/pages/Fluxo_Pietro/alarmes.dart
+++ lib/pages/Fluxo_Pietro/alarmes.dart
@@ -1,7 +1,7 @@
-import 'package:estaciona_facil/components/modal_confirmacao.dart';
 import 'package:estaciona_facil/components/widget_label.dart';
-import 'package:estaciona_facil/pages/Fluxo_Pietro/minha_conta_dois.dart';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Alarmes extends StatefulWidget {
   const Alarmes({super.key});
@@ -9,6 +9,9 @@ class Alarmes extends StatefulWidget {
   @override
   State<Alarmes> createState() => _AlarmesState();
 }
+
+String groupValue = 'Yes';
+String groupValueDois = 'Yes';
 
 class _AlarmesState extends State<Alarmes> {
   @override
@@ -21,167 +24,219 @@ class _AlarmesState extends State<Alarmes> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              WidgetLabel(texto: 'Minha Conta'),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: Row(
-                      spacing: 4,
-                      children: [
-                        Icon(
-                          Icons.person,
-                          color: Color.fromARGB(255, 42, 42, 42),
-                          size: 30,
-                        ),
-                        Text(
-                          'Dados Gerais',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 42, 42, 42),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MinhaContaDois(),
-                        ),
-                      );
-                    },
-                    child: Row(
-                      spacing: 5,
-                      children: [
-                        Icon(
-                          Icons.edit_square,
-                          color: Color.fromARGB(255, 42, 42, 42),
-                          size: 25,
-                        ),
-                        Text(
-                          'Alterar Senha',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 42, 42, 42),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+              WidgetLabel(texto: 'Alarmes'),
+              const SizedBox(height: 30),
+              Text(
+                'Quando ativar o alarme?',
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              const SizedBox(height: 15),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 28),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Radio(
+                            value: "Now",
+                            activeColor:
+                                Theme.of(context).colorScheme.secondary,
+                            groupValue: groupValue,
+                            onChanged: (value) {
+                              setState(() {
+                                groupValue = value!;
+                              });
+                            },
+                          ),
+                          Text(
+                            "Na hora em que o cartão expirar",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: GoogleFonts.ubuntu().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Radio(
+                            value: "5min",
+                            activeColor:
+                                Theme.of(context).colorScheme.secondary,
+                            groupValue: groupValue,
+                            onChanged: (value) {
+                              setState(() {
+                                groupValue = value!;
+                              });
+                            },
+                          ),
+                          Text(
+                            "5 minutos antes de expirar",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: GoogleFonts.ubuntu().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Radio(
+                            value: "10min",
+                            activeColor:
+                                Theme.of(context).colorScheme.secondary,
+                            groupValue: groupValue,
+                            onChanged: (value) {
+                              setState(() {
+                                groupValue = value!;
+                              });
+                            },
+                          ),
+                          Text(
+                            "10 minutos antes de expirar",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: GoogleFonts.ubuntu().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Radio(
+                            value: "30min",
+                            activeColor:
+                                Theme.of(context).colorScheme.secondary,
+                            groupValue: groupValue,
+                            onChanged: (value) {
+                              setState(() {
+                                groupValue = value!;
+                              });
+                            },
+                          ),
+                          Text(
+                            "30 minutos antes de expirar",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: GoogleFonts.ubuntu().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
               Container(
                 width: MediaQuery.of(context).size.width * 1,
-                height: MediaQuery.of(context).size.height * 0.005,
+                height: MediaQuery.of(context).size.height * 0.003,
                 decoration: BoxDecoration(
                   color: Theme.of(context).colorScheme.onPrimary,
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              const SizedBox(height: 30),
-              Text('Meus Dados', style: Theme.of(context).textTheme.titleLarge),
-              const SizedBox(height: 30),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+              const SizedBox(height: 20),
+              Text(
+                'Qual a duração do som do alarme?',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 28),
+                child: Column(
                   children: [
-                    Center(
-                      child: Column(
-                        spacing: 15,
-                        children: [
-                          Icon(
-                            Icons.person,
-                            color: Color.fromARGB(255, 42, 42, 42),
-                            size: 30,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Radio(
+                          value: "Curta",
+                          activeColor: Theme.of(context).colorScheme.secondary,
+                          groupValue: groupValueDois,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValueDois = value!;
+                            });
+                          },
+                        ),
+                        Text(
+                          "Curta",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: GoogleFonts.ubuntu().fontFamily,
                           ),
-
-                          Icon(
-                            Icons.email,
-                            color: Color.fromARGB(255, 42, 42, 42),
-                            size: 30,
-                          ),
-
-                          Icon(
-                            Icons.call,
-                            color: Color.fromARGB(255, 42, 42, 42),
-                            size: 30,
-                          ),
-
-                          Icon(
-                            Icons.contact_emergency,
-                            color: Color.fromARGB(255, 42, 42, 42),
-                            size: 28,
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 15),
-                    Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        spacing: 20,
-                        children: [
-                          Text(
-                            'Nome do usuário',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 42, 42, 42),
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: 'Arsenal',
-                            ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Radio(
+                          value: "Media",
+                          activeColor: Theme.of(context).colorScheme.secondary,
+                          groupValue: groupValueDois,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValueDois = value!;
+                            });
+                          },
+                        ),
+                        Text(
+                          "Média (padrão)",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: GoogleFonts.ubuntu().fontFamily,
                           ),
-
-                          Text(
-                            'Email do usuário',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 42, 42, 42),
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: 'Arsenal',
-                            ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Radio(
+                          value: "Longa",
+                          activeColor: Theme.of(context).colorScheme.secondary,
+                          groupValue: groupValueDois,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValueDois = value!;
+                            });
+                          },
+                        ),
+                        Text(
+                          "Longa",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: GoogleFonts.ubuntu().fontFamily,
                           ),
-
-                          Text(
-                            'Numero de telefone',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 42, 42, 42),
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: 'Arsenal',
-                            ),
-                          ),
-
-                          Text(
-                            'CPF ou CNPJ',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 42, 42, 42),
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
-              const SizedBox(height: 90),
-              ModalConfirmacao(texto: 'Salvar'),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.8,
-                height: MediaQuery.of(context).size.height * 0.222,
+                height: MediaQuery.of(context).size.height * 0.21,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
