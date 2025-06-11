a9dd132052307122195b2dedd72157d51f6a13bc
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2025-06-11 16:53:56 -0300

    Atualização
    home arrumada.

--- lib/pages/Fluxo_Manu/fluxo_login/home.dart
+++ lib/pages/Fluxo_Manu/fluxo_login/home.dart
@@ -15,99 +15,115 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-          child: Column(
-            spacing: 20,
-            children: [
-              AppbarHome(texto: 'Início'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Olá, ', style: TextStyle(fontSize: 30)),
-                  Text(
-                    'Nome do Usuário',
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Theme.of(context).colorScheme.onPrimary,
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+        child: Column(
+          spacing: 20,
+          children: [
+            AppbarHome(texto: 'Início'),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Olá, ', style: TextStyle(fontSize: 30)),
+                        Text(
+                          'Nome do Usuário',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          'assets/images/Logo.png',
+                          width: 90,
+                          height: 90,
+                        ),
+                        ContainerValores(nome: 'Saldo', valor: '0'),
+                      ],
+                    ),
+                    Divider(thickness: 2),
+      
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 10,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 10,
+                          children: [
+                            BotaoInicio(
+                              icone: Icons.local_parking,
+                              texto: 'Estacionar',
+                            ),
+                            BotaoInicio(
+                              icone: Icons.directions_car,
+                              texto: 'Veículos',
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 10,
+                          children: [
+                            BotaoInicio(icone: Icons.person, texto: 'Perfil'),
+                            BotaoInicio(
+                              icone: Icons.account_balance_wallet,
+                              texto: 'Saldo',
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 10,
+                          children: [
+                            BotaoInicio(
+                              icone: Icons.history,
+                              texto: 'Histórico',
+                            ),
+                            BotaoInicio(
+                              icone: Icons.notifications_active,
+                              texto: 'Alarmes',
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 10,
+                          children: [
+                            BotaoInicio(icone: Icons.help, texto: 'Ajuda'),
+                            BotaoInicio(
+                              icone: Icons.build,
+                              texto: 'Regularizar',
+                              pagina: HistoricoRegularizar(),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 10,
+                          children: [
+                            BotaoInicio(
+                              icone: Icons.description,
+                              texto: 'Termos',
+                            ),
+                            BotaoInicio(icone: Icons.logout, texto: 'Sair'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset('assets/images/Logo.png', width: 90, height: 90),
-                  ContainerValores(nome: 'Saldo', valor: '0'),
-                ],
-              ),
-              Divider(thickness: 2),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 10,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      BotaoInicio(
-                        icone: Icons.local_parking,
-                        texto: 'Estacionar',
-                      ),
-                      BotaoInicio(
-                        icone: Icons.directions_car,
-                        texto: 'Veículos',
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      BotaoInicio(icone: Icons.person, texto: 'Perfil'),
-                      BotaoInicio(
-                        icone: Icons.account_balance_wallet,
-                        texto: 'Saldo',
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      BotaoInicio(icone: Icons.history, texto: 'Histórico'),
-                      BotaoInicio(
-                        icone: Icons.notifications_active,
-                        texto: 'Alarmes',
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      BotaoInicio(icone: Icons.help, texto: 'Ajuda'),
-                      BotaoInicio(
-                        icone: Icons.build,
-                        texto: 'Regularizar',
-                        pagina: HistoricoRegularizar(),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      BotaoInicio(icone: Icons.description, texto: 'Termos'),
-                      BotaoInicio(icone: Icons.logout, texto: 'Sair'),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
