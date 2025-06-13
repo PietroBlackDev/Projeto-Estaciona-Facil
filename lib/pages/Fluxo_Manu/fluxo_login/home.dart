33c6561791029da4d2a3e8479f1dfcd47c68bc25
Henrique <henribeirox@gmail.com>
2025-06-13 20:25:33 -0300

    Update home.dart

--- lib/pages/Fluxo_Manu/fluxo_login/home.dart
+++ lib/pages/Fluxo_Manu/fluxo_login/home.dart
@@ -50,7 +50,7 @@ class _HomeState extends State<Home> {
                       ],
                     ),
                     Divider(thickness: 2),
-      
+
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       spacing: 10,
